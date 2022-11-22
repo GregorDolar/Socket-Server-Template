adbed9dcf92f5a0fc731fcbd7b3e6bcf77a7b571
Gregor Dolar <gregor.dolar@gmail.com>
2022-11-22 02:32:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 //const wss = new WebSocketServer({ port: 8080 });
 
 var server = require("ws").Server;
-var wss = new server({ port: 8080 });
+var wss = new server({ port: 80 });
 
 wss.on('connection', function connection(ws) {
 
